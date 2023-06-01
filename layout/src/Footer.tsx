e3008248e452858e4ea0dd007a1b518cab49bb47
RaviSingnath <ravisingnath@gmail.com>
2023-06-01 22:51:54 +0530

    Initial Commit

---
+++ layout/src/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+  return(
+    <footer className="bg-white text-center mt-auto">
+      <div className="text-gray-400 text-center p-4">
+        @ 2023 Copyright:
+        <a className="text-gray-500" href="#">
+          Click & Cook
+        </a>
+      </div>
+    </footer>
+  )
+}
---
+++ pages/src/data.js
@@ -0,0 +1,23 @@
+export const recipes = [
+  {
+    slug: "matar-paneer",
+    title: "Matar Paneer",
+    description: "Matar paneer recipe is a popular Indian Curry dish made with green peas and Paneer (Indian cottage cheese) in a base of onions, tomatoes, cashews, spices and herbs. The recipe hails from the versatile Northern Indian cuisine that has many different (and delicious) variations. I’m sharing our family recipe of a flavorful and simple home-style mutter paneer that you are going to like.",
+    image: "https://www.vegrecipesofindia.com/wp-content/uploads/2021/02/matar-paneer-0.jpg",
+    imageTitle: "Dassana Amit"
+  },
+  {
+    slug: "palak-paneer",
+    title: "Palak Paneer",
+    description: "Palak Paneer is one of the most popular Indian dish made with succulent Paneer cubes (Indian cottage cheese) in a smooth spinach sauce. I share two ways to make this healthy dish – a homestyle version and a restaurant style version with a charcoal smoking method (brought back on reader’s demand). Both the vibrant green palak paneer recipes are made with fresh spinach leaves (palak in Hindi), aromatics, herbs and spices. Make these super delicious spinach paneer recipes with my easy step-by-step photo guide and video.",
+    image: "https://www.vegrecipesofindia.com/wp-content/uploads/2021/06/palak-paneer-1.jpg",
+    imageTitle: "Dassana Amit"
+  },
+  {
+    slug: "paneer-butter-masala",
+    title: "Paneer Butter Masala",
+    description: "Paneer Butter Masala is one of India’s most popular paneer gravy recipes, and with good reason! Indian cottage cheese cubes are smothered in a creamy, lightly spiced tomato sauce that is downright delicious. With my video and step-by-step guide you can easily make this restaurant style Paneer Butter Masala recipe at home!",
+    image: "https://www.vegrecipesofindia.com/wp-content/uploads/2020/01/paneer-butter-masala-1.jpg",
+    imageTitle: "Dassana Amit"
+  }
+];
